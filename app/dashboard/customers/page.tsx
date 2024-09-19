51e55ca9f8f4d7f434373045205053a9a5f73667
杨宇翔 <ethan@yangyuxiangdeMacBook-Air.local>
2024-09-19 10:38:28 +0800

    feat: 完成学习课程

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomers, fetchFilteredCustomers, fetchInvoicesPages } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <CustomersTable customers={customers}/>
+  );
+}
